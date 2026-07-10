195dc9114067fa30215b861b8acb0d4d4fe7c076
Arman mohammadpour <Armanmohammadpour215@gmail.com>
2026-07-10 20:14:38 +0330

    fix new model bugs

--- new_model/problem.py
+++ new_model/problem.py
@@ -14,24 +14,24 @@ class Params:
 params = Params(
     t_sin=55.0,
     A_s=8.64,
-    A_o=0.0118,
+    A_o=0.025,
     A_e=2000.0,
     H=4.0,
 )
 
 x0 = [0.1, 
-      0.1, 
-      15.0
+      0.5, 
+      30.0
 ]
 
-u = [20, 
-     100
+u = [60, 
+     80
 ]  
 
 d = [    
     20,    # T_f - feed temperature (°C)
     4,     # x_f - feed salinity (wt% or fraction)
-    10,     # W_bin - brine inlet flow (kg/s)
+    50,     # W_bin - brine inlet flow (kg/s)
     6,     # x_bin - brine inlet salinity (fraction)
     40,    # T_bin - brine inlet temperature (°C)] 
 ] 
