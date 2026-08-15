61747cfed4a0de5210382b913004db48b9055e74
Arman mohammadpour <Armanmohammadpour215@gmail.com>
2026-08-15 11:33:22 +0330

    add enthalpy balance block

---
+++ more_accurate_model/fuzzy_model/enthalpy_balance_block.py
@@ -0,0 +1,29 @@
+import rule_activation as ra
+
+rule_table = [
+    ["Z", "NS", "NL", "NL", "NL"],
+    ["PS", "Z", "NS", "NL", "NL"],
+    ["NL", "NS", "Z", "PS", "PL"],
+    ["PL", "PL", "PS", "Z", "NS"],
+    ["PL", "PL", "PL", "PS", "Z"],
+]
+
+# E_h_in
+input1_range = [60670.99, 30000, 90000]
+# E_h_out
+input2_range = [59624.67, 53000, 66300]
+#E_h
+output_range = [0, -1000, 1000]
+
+input1_crisp_value = 40000
+input2_crisp_value = 64000
+
+e_h = ra.calculate_fuzzy_block_output(
+    input1_crisp_value,
+    input2_crisp_value,
+    input1_range,
+    input2_range,
+    output_range,
+    rule_table,
+)
+print(e_h)
